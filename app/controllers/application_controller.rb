40e6ae40d01b2d6530c3836305f615da875d2779
Chris Harvey <chharvey@icloud.com>
2018-02-20 10:31:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,23 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  get '/recipes' do
+    @recipes = Post.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipes = Post.create(
+    :name => params[:name]),
+    :ingredients => params[:ingredients]),
+    :cook_time => params[:cook_time])
+  end
+
+
+
 end
